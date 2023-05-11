d64cf9155dcf2c70bcd4f304ea5b652b519b509d
SillyMeTimbers <70308288+SillyMeTimbers@users.noreply.github.com>
2023-05-11 16:35:27 -0400

    Create TestingCode.user.js

---
+++ TestingCode.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         [Testing] Testing Realtime Updating code
+// @namespace    http://tampermonkey.net/
+// @version      5.9.310P
+// @description  null
+// @author       You
+// @match        https://amt.uhaul.net/*/Dashboard
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=uhaul.net
+// @grant        none
+// ==/UserScript==
+
+console.log("Running")
