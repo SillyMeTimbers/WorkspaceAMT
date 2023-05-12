de423330c526b9510947e4467046a0205dd5731d
SillyMeTimbers <70308288+SillyMeTimbers@users.noreply.github.com>
2023-05-12 16:35:13 -0400

    Create Settings.user.js

---
+++ Settings.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         [Testing] Script Settings
+// @namespace    http://tampermonkey.net/
+// @version      null
+// @description  settings menu
+// @author       You
+// @match        https://amt.uhaul.net/*/Dashboard
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=uhaul.net
+// @grant        none
+// ==/UserScript==
+
+const UpdateScripts = 5 // Seconds
