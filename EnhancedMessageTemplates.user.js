beebb11c5979824083f64b051ae1866b469429f9
SillyMeTimbers <70308288+SillyMeTimbers@users.noreply.github.com>
2023-05-14 23:35:42 -0400

    Update EnhancedMessageTemplates.user.js

--- EnhancedMessageTemplates.user.js
+++ EnhancedMessageTemplates.user.js
@@ -580,13 +580,13 @@ ${MessageEnd}`;
 
                 if (style === "Regular") {
                     NewMsg = `U-Haul Reservation: #${dynamicValues.resNumber} : ${dynamicValues.cxLastName}
-Your pick up has been updated! You can find your equipment available to pickup at ${dynamicValues.pickupBusinessName}, ${dynamicValues.pickupStreet}, ${dynamicValues.pickupState} ${dynamicValues.pickupZipcode} on ${dynamicValues.pickupDay}, ${dynamicValues.pickupMonthNum} ${dynamicValues.pickupDayNum}, ${dynamicValues.pickupYear} at ${dynamicValues.pickupHour}:${dynamicValues.pickupMinute} ${dynamicValues.pAMPM}.
+Your pick up has been updated! You can find your equipment available to pickup at ${dynamicValues.pickupBusinessName}, ${dynamicValues.pickupStreet}, ${dynamicValues.pickupState} ${dynamicValues.pickupState} ${dynamicValues.pickupZipcode} on ${dynamicValues.pickupDay}, ${dynamicValues.pickupMonthNum} ${dynamicValues.pickupDayNum}, ${dynamicValues.pickupYear} at ${dynamicValues.pickupHour}:${dynamicValues.pickupMinute} ${dynamicValues.pAMPM}.
 If you have any questions or concerns regarding your new pick up location, please call U-Haul Regional Scheduling Office.
 ${MessageEnd}`;
           } else if (style === "Lack of availability") {
-              NewMsg = `U-Haul Reservation: #${dynamicValues.resNumber} : ${dynamicValues.cxLastName}
-Unfortunately, your pick up has been rescheduled to the nearest location with the equipment available. You can find your equipment at ${dynamicValues.pickupBusinessName}, ${dynamicValues.pickupStreet}, ${dynamicValues.pickupState} ${dynamicValues.pickupZipcode} on ${dynamicValues.pickupDay}, ${dynamicValues.pickupMonthNum} ${dynamicValues.pickupDayNum}, ${dynamicValues.pickupYear} at ${dynamicValues.pickupHour}:${dynamicValues.pickupMinute} ${dynamicValues.pAMPM}.
-We apologize for any inconveniences this may cause for you, if you have any questions, please call U-Haul Regional Scheduling Office.
+              NewMsg = `U-Haul Reservation: #${dynamicValues.resNumber} for ${dynamicValues.cxLastName}
+We regret to inform you that your pick-up location has been updated due to equipment availability. Your equipment will be ready for pick-up at ${dynamicValues.pickupBusinessName}, ${dynamicValues.pickupStreet}, ${dynamicValues.pickupState} ${dynamicValues.pickupCity} ${dynamicValues.pickupZipcode} on ${dynamicValues.pickupMonthNum}/${dynamicValues.pickupDayNum}/${dynamicValues.pickupYear} at ${dynamicValues.pickupHour}:${dynamicValues.pickupMinute} ${dynamicValues.pAMPM}.
+We apologize for any inconvenience caused by this change. If you require assistance, need information on alternative equipment sizes, or wish to reschedule, do not hesitate to contact the U-Haul Regional Scheduling Office.
 ${MessageEnd}`;
           }
 
