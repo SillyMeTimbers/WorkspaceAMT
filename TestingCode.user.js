5684396a634e28923b77285a462545cedf08d5ee
SillyMeTimbers <70308288+SillyMeTimbers@users.noreply.github.com>
2023-05-18 19:25:29 -0400

    Update TestingCode.user.js

--- TestingCode.user.js
+++ TestingCode.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name         [Ultra-Testing] 
+// @name         [Ultra-Testing]
 // @namespace    http://tampermonkey.net/
 // @version      no
 // @description  Shows full list of company extensions
 // @author       You
 // @match        https://amt.uhaul.net/*/Dashboard
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=uhaul.net
-// @grant        GM_xmlhttpRequest
+// @grant        none
 // ==/UserScript==
 
 const Redirects = [
@@ -18,24 +18,20 @@ const Redirects = [
   "EnhancedMessageTemplates",
 ];
 
-(function() {
+(async function() {
   'use strict';
   const UpdateVal = 1;
   const now = new Date();
   const minutes = Math.floor(now.getMinutes() / UpdateVal) * UpdateVal;
   const timeString = now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate() + '-' + now.getHours() + ':' + minutes;
 
-  Redirects.forEach(redirect => {
+  for (const redirect of Redirects) {
     const url = `https://raw.githubusercontent.com/SillyMeTimbers/WorkspaceAMT/main/${redirect}.user.js?time=${timeString}`;
+    const response = await fetch(url);
+    const scriptText = await response.text();
 
-    GM_xmlhttpRequest({
-      method: "GET",
-      url: url,
-      onload: function(response) {
-        const script = document.createElement('script');
-        script.textContent = response.responseText;
-        document.head.appendChild(script);
-      }
-    });
-  });
+    const script = document.createElement('script');
+    script.textContent = scriptText;
+    document.head.appendChild(script);
+  }
 })();
