c85e3cb1fafd8f727834555ce4be70a40357cd53
SillyMeTimbers <70308288+SillyMeTimbers@users.noreply.github.com>
2023-05-18 20:19:26 -0400

    Update EnhancedMessageTemplates.user.js

--- EnhancedMessageTemplates.user.js
+++ EnhancedMessageTemplates.user.js
@@ -8,7 +8,7 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=uhaul.net
 // @grant        none
 // ==/UserScript==
-console.log("Running [Custom Message Templates] Build #1")
+console.log("Running [Custom Message Templates] Build #2")
 
 const MessageEnd = "U-Haul Co. Palm Bay, FL 561-638-9428";
 function getDynamicValuesForTemplate(templateName) {
