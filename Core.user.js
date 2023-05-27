3784535660930b3be4dfa2fa6d06e0b97447a5d0
SillyMeTimbers <70308288+SillyMeTimbers@users.noreply.github.com>
2023-05-26 23:35:28 -0400

    Update Core.user.js

--- Core.user.js
+++ Core.user.js
@@ -1,22 +1,13 @@
 // ==UserScript==
-// @name         [Ultra-Testing]
+// @name         Handler
 // @namespace    http://tampermonkey.net/
 // @version      no
-// @description  Shows full list of company extensions
+// @description  
 // @author       You
 // @match        https://amt.uhaul.net/*/Dashboard
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=uhaul.net
 // @grant        none
 // ==/UserScript==
-
-const Redirects = [
-  "ExtensionList",
-  "ExpNotes_VerifyTime",
-  "TopBarAlert",
-  "OverdueOpenInPOS",
-  "SendDropoffButtons",
-  "EnhancedMessageTemplates",
-];
 
 (async function() {
   'use strict';
@@ -26,7 +17,7 @@ const Redirects = [
   const timeString = now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate() + '-' + now.getHours() + ':' + minutes;
 
   for (const redirect of Redirects) {
-    const url = `https://raw.githubusercontent.com/SillyMeTimbers/WorkspaceAMT/Live/${redirect}.user.js?time=${timeString}`;
+    const url = `https://raw.githubusercontent.com/SillyMeTimbers/WorkspaceAMT/main/Core.user.js?time=${timeString}`;
     const response = await fetch(url);
     const scriptText = await response.text();
 
