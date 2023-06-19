8a17e45956df7fa4e6914b8b79f23a111e97323c
SillyMeTimbers <70308288+SillyMeTimbers@users.noreply.github.com>
2023-06-19 13:56:36 -0400

    Update ExpNotes_VerifyTime.user.js

--- ExpNotes_VerifyTime.user.js
+++ ExpNotes_VerifyTime.user.js
@@ -8,7 +8,7 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=uhaul.net
 // @grant        none
 // ==/UserScript==
-const VerifyReturnVersion = "8"
+const VerifyReturnVersion = "9"
 
 // Styles
 function injectCSS(css) {
