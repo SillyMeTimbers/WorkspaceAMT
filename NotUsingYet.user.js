fac82e6f74b6a18610580bc25fb6be7e557c368e
SillyMeTimbers <70308288+SillyMeTimbers@users.noreply.github.com>
2023-07-15 23:14:41 -0400

    new stuff

--- NotUsingYet.user.js
+++ NotUsingYet.user.js
@@ -51,6 +51,71 @@ const MsgTemplates = {
     },
 }
 
+function createSubDropdown(id, data) {
+    let container = document.createElement('div');
+    container.className = "template-container";
+
+    for (let key in data) {
+        let selectId = id + key;
+
+        let label = document.createElement('label');
+        label.className = "template-label";
+        label.textContent = `${data[key].DisplayText}:`;
+
+        let select = document.createElement('select');
+        select.id = selectId;
+        select.name = selectId;
+        select.className = "hidden-field";
+
+        data[key].Options.forEach(option => {
+            let optionElement = document.createElement('option');
+            optionElement.value = option.value;
+            optionElement.textContent = option.text;
+
+            if (option.value === data[key].DefaultOption) {
+                optionElement.selected = true;
+            }
+
+            select.appendChild(optionElement);
+        });
+
+        label.appendChild(select);
+
+        // The dropdown visuals
+        let dropdown = document.createElement('div');
+        dropdown.className = "custom dropdown msgcorner";
+
+        let current = document.createElement('a');
+        current.href = "#";
+        current.className = "current";
+        current.textContent = data[key].DisplayText;
+        dropdown.appendChild(current);
+
+        let selector = document.createElement('a');
+        selector.href = "#";
+        selector.className = "selector";
+        dropdown.appendChild(selector);
+
+        let ul = document.createElement('ul');
+        ul.className = "msgdropdown";
+        data[key].Options.forEach(option => {
+            let li = document.createElement('li');
+            li.textContent = option.text;
+
+            if (option.value === data[key].DefaultOption) {
+                li.className = "selected";
+            }
+
+            ul.appendChild(li);
+        });
+        dropdown.appendChild(ul);
+        label.appendChild(dropdown);
+        container.appendChild(label);
+    }
+
+    return container;
+}
+
 function isMessageTextForumVisible() {
     const textSubmitForm = document.querySelector("#textMessageArea");
     if (
@@ -80,13 +145,31 @@ async function waitForElement(selector, timeout = 10000) {
     return null;
 }
 
+function AddMessageTemplate(n, t) {
+    t.val(n.val())
+}
+
+function formatPhoneNumber(inputElement) {
+    let value = inputElement.value.replace(/\D/g, '');
+    if (value.length > 10) value = value.slice(0, 10); // Limit to 10 numerical characters
+
+    let formattedValue = '';
+    if (value.length > 0) formattedValue += '(' + value.slice(0, 3);
+    if (value.length > 3) formattedValue += ') ' + value.slice(3, 6);
+    if (value.length > 6) formattedValue += '-' + value.slice(6);
+    inputElement.value = formattedValue;
+}
+
+window.AddMessageTemplate = AddMessageTemplate;
+window.formatPhoneNumber = formatPhoneNumber;
+
 function MessageTextForumVisible() {
     if (MessageTemplateLastVisible === false) {
         MessageTemplateLastVisible = true;
-        console.log("Prompted")
 
         const MessagePopup = waitForElement("Body > #SecondaryPopup > #textSubmitForm", 5000)
         if (MessagePopup) {
+            const ClonePhoneNumber = document.querySelector("#CustomerPhoneNumber").value
             document.querySelector("Body > #SecondaryPopup").style.borderRadius = '10px';
 
             const nMessagePopup = document.querySelector("Body > #SecondaryPopup > #textSubmitForm")
@@ -102,7 +185,7 @@ function MessageTextForumVisible() {
                     <div class="msgleft">
                         <label class="phonenumber-label">
                             Phone Number:
-                            <input id="CustomerPhoneNumber" name="CustomerPhoneNumber" type="text" value="(999) 999-9999" class="phone-input">
+                            <input id="CustomerPhoneNumber" name="CustomerPhoneNumber" type="text" value="${ClonePhoneNumber}" class="phone-input">
                         </label>
                 
                         <li class="templatesplit"></li>
@@ -128,7 +211,7 @@ function MessageTextForumVisible() {
                             <label class="template-label">
                                 Option 1:
                 
-                                <select id="customCustomerContactTemplateDropdown mainDropdown" name="GetCustomCustomerContactTemplate" class="hidden-field">
+                                <select id="customCustomerContactTemplateDropdown" name="GetCustomCustomerContactTemplate" class="hidden-field">
                                     <option value="This is an option">Testing Option 1</option>
                                     <option value="This is an option">Testing Option 2</option>
                                     <option value="This is an option">Testing Option 3</option>
@@ -155,14 +238,21 @@ function MessageTextForumVisible() {
                             </label>
                         </div>
                     
-                        <button type="button" class="right msgcorner templateadd" onclick="AppendCustomCustomerContactTemplate($('#mainDropdown'), $('#textMessageArea'))">Add Template</button>
+                        <button type="button" class="right msgcorner templateadd" onclick="AddMessageTemplate($('#mainTemplateList #customCustomerContactTemplateDropdown'), $('#textMessageArea'))">Add Template</button>
                     </div>
                 
                     <div class="msgright">
+                        Note Content:
+
+                        <div class="msgBox">
+                            <textarea placeholder="Placeholder text" id="noteMessageArea" name="NoteMessage" class="message-textarea"></textarea>
+                        </div>
+
+                        <li class="templatesplit" style="opacity: 0;"></li>
                         Message Content:
 
                         <div class="msgBox">
-                            <textarea placeholder="Text message go here..." id="textMessageArea" name="TextMessage" class="message-textarea"></textarea>
+                            <textarea placeholder="Placeholder text" id="textMessageArea" name="TextMessage" class="message-textarea"></textarea>
                         </div>
                     </div>
                 </div>
@@ -276,7 +366,7 @@ function MessageTextForumVisible() {
                     margin-bottom: 0px !important;
                 }   
                 
-                .msgleft .templatesplit {
+                .messagecontent .templatesplit {
                   border-bottom: none;
                   border-top: solid 1px #d6d6d6;
                   clear: both;
@@ -284,7 +374,7 @@ function MessageTextForumVisible() {
                   padding-bottom: 10px;
                 }
 
-                .msgleft .templatesplit::marker {
+                .messagecontent .templatesplit::marker {
                     content: "";
                 }
 
@@ -317,6 +407,112 @@ function MessageTextForumVisible() {
 
             const imageSrc = "https://cdn.discordapp.com/attachments/962895897434394674/1090722644254535760/DynamicMessageColorSlightlyThicker.png";
 
+            // Phone Number formatting
+            const phoneNumberInput = document.querySelector("#CustomerPhoneNumber");
+            function formatPhoneNumber(inputElement) {
+                let value = inputElement.value.replace(/\D/g, '');
+                if (value.length > 10) value = value.slice(0, 10); // Limit to 10 numerical characters
+
+                let formattedValue = '';
+                if (value.length > 0) formattedValue += '(' + value.slice(0, 3);
+                if (value.length > 3) formattedValue += ') ' + value.slice(3, 6);
+                if (value.length > 6) formattedValue += '-' + value.slice(6);
+                inputElement.value = formattedValue;
+            }
+
+            phoneNumberInput.addEventListener('input', function () {
+                formatPhoneNumber(this);
+            });
+
+            formatPhoneNumber(phoneNumberInput);
+
+            function handleTemplateDropdownChange(event) {
+                console.log("Dropdown Changed")
+                const DropdownMenu = document.querySelector("#mainTemplateList .msgdropdown")
+                const Selected = DropdownMenu.querySelector(".selected").textContent.trim()
+                const SubMenu = document.querySelector(".template-indent")
+                SubMenu.innerHTML = ``
+
+                console.log(Selected)
+                if (Selected == "Equipment Change") {
+                    const NewDropdown = createSubDropdown("EquipmentChange", {
+                        "OldEquipment": {
+                            DisplayText: "Previous Equipment",
+                            DefaultOption: "",
+                            Options: [
+                                { value: "", text: "" },
+                                { value: "BE", text: "BE - Cargo Van" },
+                                { value: "BP", text: "BP - Pickup Truck" },
+                                { value: "MP", text: "MP - Ford Maverick Pickup Truck" },
+                                { value: "TM", text: "TM - 10' Box Truck" },
+                                { value: "DC", text: "DC - 15' Box Truck" },
+                                { value: "EL", text: "EL - 17' Box Truck" },
+                                { value: "TT", text: "TT - 20' Box Truck" },
+                                { value: "JH", text: "JH - 26' Box Truck" },
+                                { value: "FS", text: "FS - 4' X 7' Open Trailer" },
+                                { value: "AO", text: "AO - 5' X 8' Open Trailer" },
+                                { value: "RO", text: "RO - 6' X 12' Open Trailer" },
+                                { value: "HO", text: "HO - 6' X 12' Open Trailer w/Ramp" },
+                                { value: "UV", text: "UV - 4' X 8' Enclosed Trailer" },
+                                { value: "AV", text: "AV - 5' X 8' Enclosed Trailer" },
+                                { value: "MV", text: "MV - 5' X 10' Enclosed Trailer" },
+                                { value: "RV", text: "RV - 6' X 12' Enclosed Trailer" },
+                                { value: "RT", text: "RT - 5' X 9' Open Trailer w/Ramp" },
+                                { value: "MT", text: "MT - Motorcycle Trailer" },
+                                { value: "TD", text: "TD - Tow Dolly" },
+                                { value: "AT", text: "AT - Auto Transport" },
+                                { value: "AA", text: "AA - Wooden U-Box" },
+                                { value: "AB", text: "AB - Plastic U-Box" },
+                            ]
+                        },
+
+                        "NewEquipment": {
+                            DisplayText: "New Equipment",
+                            DefaultOption: "",
+                            Options: [
+                                { value: "", text: "" },
+                                { value: "BE", text: "BE - Cargo Van" },
+                                { value: "BP", text: "BP - Pickup Truck" },
+                                { value: "MP", text: "MP - Ford Maverick Pickup Truck" },
+                                { value: "TM", text: "TM - 10' Box Truck" },
+                                { value: "DC", text: "DC - 15' Box Truck" },
+                                { value: "EL", text: "EL - 17' Box Truck" },
+                                { value: "TT", text: "TT - 20' Box Truck" },
+                                { value: "JH", text: "JH - 26' Box Truck" },
+                                { value: "FS", text: "FS - 4' X 7' Open Trailer" },
+                                { value: "AO", text: "AO - 5' X 8' Open Trailer" },
+                                { value: "RO", text: "RO - 6' X 12' Open Trailer" },
+                                { value: "HO", text: "HO - 6' X 12' Open Trailer w/Ramp" },
+                                { value: "UV", text: "UV - 4' X 8' Enclosed Trailer" },
+                                { value: "AV", text: "AV - 5' X 8' Enclosed Trailer" },
+                                { value: "MV", text: "MV - 5' X 10' Enclosed Trailer" },
+                                { value: "RV", text: "RV - 6' X 12' Enclosed Trailer" },
+                                { value: "RT", text: "RT - 5' X 9' Open Trailer w/Ramp" },
+                                { value: "MT", text: "MT - Motorcycle Trailer" },
+                                { value: "TD", text: "TD - Tow Dolly" },
+                                { value: "AT", text: "AT - Auto Transport" },
+                                { value: "AA", text: "AA - Wooden U-Box" },
+                                { value: "AB", text: "AB - Plastic U-Box" },
+                            ]
+                        },
+
+                        "FreeUpgrade": {
+                            DisplayText: "Free Upgrade",
+                            DefaultOption: "",
+                            Options: [
+                                { value: true, text: "Yes" },
+                                { value: false, text: "No" },
+                            ]
+                        },
+                    })
+
+                    SubMenu.appendChild(NewDropdown);
+                }
+            }
+
+            const mainDropdownChange = document.querySelector("#mainTemplateList #customCustomerContactTemplateDropdown");
+            mainDropdownChange.addEventListener("change", handleTemplateDropdownChange);
+
             function updateCurrentAnchorText() {
                 const list = document.querySelector("#mainTemplateList #customCustomerContactList");
                 const currentAnchor = document.querySelector("#textSubmitForm .current");
@@ -331,24 +527,21 @@ function MessageTextForumVisible() {
                         currentAnchor.textContent = firstListItemText;
                     }
                 }
-            } 
+            }
 
             for (const MsgName in MsgTemplates) {
                 const MsgData = MsgTemplates[MsgName]
-                const MsgDisplayName = MsgData["Display"]
+                const MsgDisplayName = MsgData.Display
 
-                console.log(MsgDisplayName)
                 if (MsgData.Params()) {
-                    // Create Options
-                    const MsgHiddenValue = new Option(MsgName)
-                    MsgHiddenValue.value = "hey"
-                    MsgHiddenValue.id = `${MsgName}:HiddenValue`
-                    MsgHiddenValue.innerHTML = `<img src="${imageSrc}" alt="Dynamic" width="16" height="16" style="vertical-align: middle; margin-right: 5px;"> <span style="color: #2A517C;">${MsgData.Display}</span>`;
-                    document.querySelector("#mainTemplateList > #customCustomerContactTemplateDropdown").appendChild(MsgHiddenValue)
-                
                     const MsgOption = document.createElement("li");
-                    MsgOption.innerHTML = `<img src="${imageSrc}" alt="Dynamic" width="16" height="16" style="vertical-align: middle; margin-right: 5px;"> <span style="color: #2A517C;">${MsgData.Display}</span>`;
+                    MsgOption.textContent = `${MsgDisplayName}`
                     document.querySelector("#mainTemplateList > div > #customCustomerContactList").appendChild(MsgOption)
+
+                    const MsgHiddenValue = new Option(MsgDisplayName, MsgName)
+                    MsgHiddenValue.value = MsgData.MsgTemplate
+                    MsgHiddenValue.id = `${MsgName}:HiddenValue`
+                    document.querySelector("#mainTemplateList > #customCustomerContactTemplateDropdown").appendChild(MsgHiddenValue)
                 }
             };
 
