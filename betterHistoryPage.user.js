c16f66ac4085a975e25f274c2276a2b390cadac3
SillyMeTimbers <70308288+SillyMeTimbers@users.noreply.github.com>
2023-09-20 15:55:38 -0400

    Update betterHistoryPage.user.js

--- betterHistoryPage.user.js
+++ betterHistoryPage.user.js
@@ -10,123 +10,124 @@
 
 let paymentCSS_StyleSheetAdded = false
 function getOrdinal(n) {
-    var s = ["th", "st", "nd", "rd"],
-        v = n % 100;
-    return n + (s[(v - 20) % 10] || s[v] || s[0]);
+	var s = ["th", "st", "nd", "rd"],
+		v = n % 100;
+	return n + (s[(v - 20) % 10] || s[v] || s[0]);
 }
 
-function convertDateFormat(s) {
-    if (!s.startsWith('Pick up date/time changed') && !s.startsWith('Due date/time changed')) {
-        return s; // Return the original string if it doesn't match the expected prefixes
-    }
+function formatChange(text) {
+	// For date changes
+	const dateMatch = text.match(/(\d{4}-\d{2}-\d{2} \d{2}:\d{2}) to (\d{4}-\d{2}-\d{2} \d{2}:\d{2})/);
+	if (dateMatch) {
+		const oldDate = new Date(dateMatch[1]);
+		const newDate = new Date(dateMatch[2]);
+		const formattedNewDate = newDate.toLocaleString('en-US', { month: 'short', day: 'numeric', year: 'numeric', hour: '2-digit', minute: '2-digit', hour12: true });
+		const formattedOldDate = oldDate.toLocaleString('en-US', { month: 'short', day: 'numeric', year: 'numeric', hour: '2-digit', minute: '2-digit', hour12: true });
+		const eventType = text.startsWith('Pick up') ? 'Pick up' : 'Due date';
+		return { text: `${eventType} date/time changed to ${formattedNewDate}`, previous: `Previously: ${formattedOldDate}` };
+	}
 
-    // Extract the last date and time from the string
-    const dateStr = s.split('to ')[1].split(' ')[0];
-    const timeStr = s.split('to ')[1].split(' ')[1];
+	// For rate changes
+	const rateMatch = text.match(/changed from (\d+.\d{2}) to (\d+(?:\.\d{2})?)/);
+	if (rateMatch) {
+		return { text: `Rental rate changed to ${parseFloat(rateMatch[2]).toFixed(2)}`, previous: `Previously: ${parseFloat(rateMatch[1]).toFixed(2)}` };
+	}
 
-    const year = parseInt(dateStr.split('-')[0]);
-    const month = parseInt(dateStr.split('-')[1]) - 1; // Months are 0-indexed in JS
-    const day = parseInt(dateStr.split('-')[2]);
-
-    const hours = parseInt(timeStr.split(':')[0]);
-    const minutes = parseInt(timeStr.split(':')[1]);
-
-    // Create a Date object using extracted parts
-    const dateObj = new Date(year, month, day, hours, minutes);
-
-    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    const formattedStr = `${monthNames[dateObj.getMonth()]} ${getOrdinal(dateObj.getDate())}, ${dateObj.getFullYear()} ${dateObj.getHours() % 12 || 12}:${(dateObj.getMinutes() < 10 ? '0' : '') + dateObj.getMinutes()} ${dateObj.getHours() >= 12 ? 'PM' : 'AM'}`;
-
-    // Determine event type (Pick up or Due)
-    const eventType = s.includes('Pick up') ? 'Pick up' : 'Due';
-
-    return `${eventType} date/time changed to ${formattedStr}`;
+	// Return the original text if no matches
+	return { text: text, previous: '' };
 }
+
 
 function updateHistoryScreen() {
-    const HistoryTableDirec = document.querySelector(`[data-slug="section7"]`);
+	const HistoryTableDirec = document.querySelector(`[data-slug="section7"]`);
 
-    if (HistoryTableDirec) {
+	if (HistoryTableDirec) {
 
-        var css = `
-.tooltip-content {
-    display: none;
-    position: absolute;
-    font-weight: normal;
-    font-size: 1em;
-    light-height: 1.3;
-    border: solid 1px #cccccc;
-    color: #555;
-    background: #fff;
-    border-radius: 3px;
-    pointer-events: none; /* Add this line */
-    padding: 5px 10px;
-    transition: opacity 0.3s;  /* Transition for fade effect */
-}
+		var css = `
+			.tooltip-content {
+				opacity: 0;
+				z-index: 999;
+				visibility: hidden; /* Initial state is hidden */
+				position: absolute;
+				font-weight: normal;
+				font-size: 1em;
+				line-height: 1.3;
+				border: solid 1px #cccccc;
+				color: #555;
+				background: #fff;
+				border-radius: 3px;
+				pointer-events: none;
+				padding: 0.5em;
+				transition: opacity 0.2s, visibility 0.2s;
+				top: 100%;
+				left: 0;
+				margin-top: 10px;
+			}
 
-.tooltip-wrapper:hover .tooltip-content {
-    display: block;
-}
+			.tooltip-wrapper:hover .tooltip-content {
+				opacity: 1;
+				visibility: visible; /* Display on hover */
+			}
 
+			.tooltip-wrapper {
+				cursor: help;
+				position: relative; /* Ensure this is set so the child's absolute positioning is relative to this parent */
+			}
             `,
-            head = document.head || document.getElementsByTagName('head')[0],
-            style = document.createElement('style');
+			head = document.head || document.getElementsByTagName('head')[0],
+			style = document.createElement('style');
 
-        if (!paymentCSS_StyleSheetAdded) {
-            paymentCSS_StyleSheetAdded = true;
+		if (!paymentCSS_StyleSheetAdded) {
+			paymentCSS_StyleSheetAdded = true;
 
-            head.appendChild(style);
-            style.type = 'text/css';
-            if (style.styleSheet) {
-                style.styleSheet.cssText = css;
-            } else {
-                style.appendChild(document.createTextNode(css));
-            }
-        }
+			head.appendChild(style);
+			style.type = 'text/css';
+			if (style.styleSheet) {
+				style.styleSheet.cssText = css;
+			} else {
+				style.appendChild(document.createTextNode(css));
+			}
+		}
 
-        const HistoryData = HistoryTableDirec.querySelector("table")
-$(`[data-slug="section7"] table tr`).each(function() {
-    if ($(this).attr("sorted") != "true") {
-        let Response = $(this).find("td:nth-child(4)");
+		const HistoryData = HistoryTableDirec.querySelector("table")
+		// Your iteration code:
+		$(`[data-slug="section7"] table tr`).each(function() {
+			if ($(this).attr("sorted") != "true") {
+				let Response = $(this).find("td:nth-child(4)");
 
-        let previousDate = Response.text().split(' to ')[0].split('changed from ')[1];
-        let formattedDate = convertDateFormat(Response.text());
+				const formattedResponse = formatChange(Response.text());
 
-        // Create tooltip structure
-        let tooltipContent = $('<span class="tooltip-content"></span>').text('Previous: ' + previousDate);
-        let tooltipWrapper = $('<div class="tooltip-wrapper"></div>').append(formattedDate, tooltipContent);
+				if (formattedResponse.previous) {  // If there's a "previous" tooltip text
+					// Create tooltip structure
+					let tooltipContent = $('<span class="tooltip-content"></span>').text(formattedResponse.previous);
+					let tooltipWrapper = $('<div class="tooltip-wrapper"></div>').append(formattedResponse.text, tooltipContent);
+					Response.empty().append(tooltipWrapper);
+				} else {
+					Response.text(formattedResponse.text);
+				}
 
-        Response.empty().append(tooltipWrapper);
-
-        $(this).attr("sorted", "true");
-    }
-});
-
-// Mouse events for fade-in and fade-out
-$(`[data-slug="section7"] table`).on('mouseenter', '.tooltip-wrapper', function() {
-    $(this).find('.tooltip-content').fadeIn(300);  // Fade in over 300ms
-}).on('mouseleave', '.tooltip-wrapper', function() {
-    $(this).find('.tooltip-content').fadeOut(300); // Fade out over 300ms
-});
-    }
+				$(this).attr("sorted", "true");
+			}
+		});
+	}
 }
 
 // Function to continuously check if the textSubmitForm is visible
 function runPaymentImprovement() {
-    function addScriptVersion(scriptName, version) {
-        let scriptVersionElement = document.createElement('div');
-        scriptVersionElement.style.display = 'none'; // Make it hidden
-        scriptVersionElement.classList.add('script-version'); // So we can find it later
-        scriptVersionElement.dataset.name = scriptName; // Store the script name
-        scriptVersionElement.dataset.version = version; // Store the version
-        document.body.appendChild(scriptVersionElement);
-    }
+	function addScriptVersion(scriptName, version) {
+		let scriptVersionElement = document.createElement('div');
+		scriptVersionElement.style.display = 'none'; // Make it hidden
+		scriptVersionElement.classList.add('script-version'); // So we can find it later
+		scriptVersionElement.dataset.name = scriptName; // Store the script name
+		scriptVersionElement.dataset.version = version; // Store the version
+		document.body.appendChild(scriptVersionElement);
+	}
 
-    addScriptVersion("Improved History Screen", "1")
+	addScriptVersion("Improved History Screen", "2")
 
-    setInterval(() => {
-        updateHistoryScreen()
-    }, 100); // Check every 100ms
+	setInterval(() => {
+		updateHistoryScreen()
+	}, 100); // Check every 100ms
 }
 
 runPaymentImprovement();
