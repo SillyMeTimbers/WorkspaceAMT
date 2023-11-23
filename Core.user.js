2eb5dfbac8dd3a305bff0a1fe7c30030fe821ebc
SillyMeTimbers <70308288+SillyMeTimbers@users.noreply.github.com>
2023-11-22 23:00:23 -0500

    Update Core.user.js

--- Core.user.js
+++ Core.user.js
@@ -32,7 +32,7 @@ const Redirects = [
 const PermissionsList = {
     "Joshua Mccart": {
         Branch: {
-            Live: false,
+            Live: true,
         },
         Exclude: [],
     },
