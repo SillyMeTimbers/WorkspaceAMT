ea30234e29482c9386a6bfdfad5b96f7ca453af5
SillyMeTimbers <70308288+SillyMeTimbers@users.noreply.github.com>
2023-12-16 19:30:05 -0500

    Update Core.user.js

--- Core.user.js
+++ Core.user.js
@@ -35,7 +35,7 @@ const PermissionsList = {
         Branch: {
             Live: false,
         },
-        Exclude: ["EnhancedMessageTemplates"],
+        Exclude: [],
     },
 
     "Emily Bertrand": {
