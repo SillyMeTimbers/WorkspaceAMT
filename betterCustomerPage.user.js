29c5cf3488db8d257972c78771ffac8904f08909
SillyMeTimbers <70308288+SillyMeTimbers@users.noreply.github.com>
2023-12-21 18:17:43 -0500

    Update betterCustomerPage.user.js

--- betterCustomerPage.user.js
+++ betterCustomerPage.user.js
@@ -32,6 +32,34 @@
 		#CustomerInformationTable {
 			border: 0px;
 		}
+
+		#CustomerInformationTable span {
+			margin: 2px;
+		}
+
+		.inputbuttons {
+			margin: 0px !important;
+		}
+
+		.inputbuttons > button {
+			margin-right: 10px !important;
+		}
+
+		.customerInfo {
+			margin: 0px !important;
+		}
+
+		.splitinfo {
+			display: flex;
+		}
+
+		.splitinfo > div {
+			width: calc(50% - 5px)
+		}
+		.splitinfo > div:nth-child(1) {
+			margin-right: 10px;
+		}
+		.
 	  `;
 	injectCSS(CSSToInject);
 
@@ -39,28 +67,47 @@
 		const CustomerTabDirect = document.querySelector(`[data-slug="section5"]`);
 		if (CustomerTabDirect && !CustomerTabDirect.querySelector("#CustomerInformation")) {
 			const CustomerTabInformation = CustomerTabDirect.cloneNode(true)
-			// const Template = $(`
-
-			// `)
-			// Template.show()
 
 			CustomerTabDirect.innerHTML = '';  // Reset Content
 			const Html_Content = `
 				<div id="CustomerInformation">
-					<table id="CustomerInformationTable">
-						<tbody>
-							<tr>
-								<td>
-									Customer
-									<input id="CustomerNameIdentifier" type="text" value="" disabled="true">
-								</td>
-							</tr>
-						</tbody>
-					</table>
-				</div>		  
-        	`
+					<div id="CustomerInformationTable">
+						<fieldset class="customerInfo">
+							<legend class="customerName">
+								John Doe
+							</legend>
+						
+							<div class="splitinfo">
+								<div>
+									<input id="customerPhone" type="text" value="(999) 999-9999" disabled="true">
+								</div>
 
-			$(`[data-slug="section5"]`).css("padding", "0px")
+								<div>
+									<input id="customerEmail" type="text" value="johndoe@uhaul.com" disabled="true">
+								</div>
+							</div>
+
+							<dl class="inline movinginfo">
+								<dt>Moving From:</dt>
+								<dd id="movingFromAddress">4703 Babcock St, Palm Bay, FL 32905</dd>
+								<dt>Moving To:</dt>
+								<dd id="movingToAddress">376 N Harbor City Blvd, Melbourne, FL 32935</dd>
+                        	</dl>
+
+							<div class="inputbuttons row">
+								<button class="left" type="button">Send Text</button>
+								<button class="left" type="button">Send Email</button>
+								<button class="left" type="button">Modify Customer</button>
+								<button class="left" type="button">Manage Customer</button>
+							</div>
+						</fieldset>
+
+						<p>
+							<a class="right" href="#" onclick="OpenAddCustomer()"><i class="fa fa-plus"></i> Add Customer</a>
+						</p>
+					</div>
+				</div>
+        	`
 			CustomerTabDirect.innerHTML = Html_Content;
 			//$("#CustomerInformationTable").append(Template)
 		}
