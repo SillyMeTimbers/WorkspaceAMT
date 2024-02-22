ace8c3161fd627ac76e1ee090e4efd76cb499cb8
SillyMeTimbers <70308288+SillyMeTimbers@users.noreply.github.com>
2024-02-22 13:54:35 -0500

    Update ExtensionList.user.js

--- ExtensionList.user.js
+++ ExtensionList.user.js
@@ -8,7 +8,7 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=uhaul.net
 // @grant        none
 // ==/UserScript==
-const ExtensionListVersion = "6"
+const ExtensionListVersion = "7"
 async function extensionButtonWaitForElement(selector, timeout = 10000) {
     const startTime = Date.now();
 
